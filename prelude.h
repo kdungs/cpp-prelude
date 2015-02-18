beb2a2540234e7cf7f75fb0eceebd6bf83ff0f6a
Kevin Dungs <kevin@dun.gs>
2015-02-18 16:26:58 +0100

    Implement everything related to list operations.

--- prelude.h
+++ prelude.h
@@ -13,9 +13,9 @@ namespace Prelude {
 #define Predicate typename
 #define Type typename
 
-/*
- * List operations
- */
+// -----------------
+//  List operations 
+// -----------------
 
 // map :: (a -> b) -> [a] -> [b]
 template <Function FN, Container CN, Type A,
@@ -30,6 +30,13 @@ auto map(const FN& f, const CN<A, AllocA>& c) -> CN<B, AllocB> {
 }
 
 // (++) :: [a] -> [a] -> [a]
+template <Container CN, Type A, typename AllocA = std::allocator<A>>
+auto join(const CN<A, AllocA>& left, const CN<A, AllocA>& right)
+    -> CN<A, AllocA> {
+  auto res = CN<A, AllocA>{left};
+  res.insert(std::end(res), std::begin(right), std::end(right));
+  return res;
+}
 
 // filter :: (a -> Bool) -> [a] -> [a]
 template <Predicate PR, Container CN, Type A,
@@ -57,9 +64,56 @@ auto last(const CN<A, AllocA>& c) -> A {
 }
 
 // tail :: [a] -> [a]
+template <Container CN, Type A, typename AllocA = std::allocator<A>>
+auto tail(const CN<A, AllocA>& c) -> CN<A, AllocA> {
+  assert(c.size() && "Container can't be empty.");
+  auto res = CN<A, AllocA>{std::begin(c) + 1, std::end(c)};
+  return res;
+}
+
 // init :: [a] -> [a]
+template <Container CN, Type A, typename AllocA = std::allocator<A>>
+auto init(const CN<A, AllocA>& c) -> CN<A, AllocA> {
+  assert(c.size() && "Container can't be empty.");
+  auto res = CN<A, AllocA>{std::begin(c), std::end(c) - 1};
+  return res;
+}
+
 // null :: [a] -> Bool
+template <typename _Container>
+auto null(const _Container& c) -> bool {
+  return c.empty();
+}
+
 // length :: [a] -> Int
+template <typename _Container>
+auto length(const _Container& c) -> std::size_t {
+  return c.size();
+}
+
 // (!!) :: [a] -> Int -> a
+template <Container CN, Type A, typename AllocA = std::allocator<A>>
+auto at(const CN<A, AllocA>& c, std::size_t pos) -> A {
+  assert(pos < c.size() && "Position has to be within size of Container.");
+  return c.at(pos);
+}
+
 // reverse :: [a] -> [a]
+template <Container CN, Type A, typename AllocA = std::allocator<A>>
+auto reverse(const CN<A, AllocA>& c) -> CN<A, AllocA> {
+  auto res = CN<A, AllocA>{c};
+  std::reverse(std::begin(res), std::end(res));
+  return res;
+}
+
+
+// ------------------------
+//  Reducing lists (folds) 
+// ------------------------
+
+// foldl :: (b -> a -> b) -> b -> [a] -> b
+// foldl1 :: (a -> a -> a) -> [a] -> a
+// foldr :: (a -> b -> b) -> b -> [a] -> b
+// foldr1 :: (a -> a -> a) -> [a] -> a
+
 }
--- test.cc
+++ test.cc
@@ -6,33 +6,88 @@
 
 auto test_map() -> void {
   using Prelude::map;
-  auto expected = std::vector<bool>{false, true, false, true, false};
+  auto expect = std::vector<bool>{false, true, false, true, false};
   auto even = [](int x) { return x % 2 == 0; };
   auto result = map(even, std::vector<int>{1, 2, 3, 4, 5});
-  assert(result == expected);
+  assert(result == expect);
+}
+
+auto test_join() -> void {
+  using Prelude::join;
+  auto expect = std::vector<int>{1, 2, 3, 4};
+  auto result = join(std::vector<int>{1, 2}, std::vector<int>{3, 4});
+  assert(result == expect);
 }
 
 auto test_filter() -> void {
   using Prelude::filter;
-  auto expected = std::vector<int>{2, 4};
+  auto expect = std::vector<int>{2, 4};
   auto even = [](int x) { return x % 2 == 0; };
   auto result = filter(even, std::vector<int>{1, 2, 3, 4, 5});
-  assert(result == expected);
+  assert(result == expect);
 }
 
 auto test_head() -> void {
   using Prelude::head;
-  assert(head(std::vector<int>{1, 2, 3, 4}) == 1);
+  auto expect = 1;
+  auto result = head(std::vector<int>{1, 2, 3, 4});
+  assert(result == expect);
 }
 
 auto test_last() -> void {
   using Prelude::last;
-  assert(last(std::vector<int>{1, 2, 3, 4}) == 4);
+  auto expect = 4;
+  auto result = last(std::vector<int>{1, 2, 3, 4});
+  assert(result == expect);
+}
+
+auto test_tail() -> void {
+  using Prelude::tail;
+  auto expect = std::vector<int>{2, 3, 4};
+  auto result = tail(std::vector<int>{1, 2, 3, 4});
+  assert(result == expect);
+}
+
+auto test_init() -> void {
+  using Prelude::init;
+  auto expect = std::vector<int>{1, 2, 3};
+  auto result = init(std::vector<int>{1, 2, 3, 4});
+  assert(result == expect);
+}
+
+auto test_null() -> void {
+  using Prelude::null;
+  assert(null(std::vector<int>{}));
+  assert(!null(std::vector<int>{1}));
+}
+
+auto test_length() -> void {
+  using Prelude::length;
+  assert(length(std::vector<int>{}) == 0);
+  assert(length(std::vector<int>{1, 2, 3, 4}) == 4);
+}
+
+auto test_at() -> void {
+  using Prelude::at;
+  assert(at(std::vector<int>{1, 2, 3, 4}, 2) == 3);
+}
+
+auto test_reverse() -> void {
+  using Prelude::reverse;
+  auto expect = std::vector<int>{4, 3, 2, 1};
+  auto result = reverse(std::vector<int>{1, 2, 3, 4});
+  assert(result == expect);
 }
 
 int main() {
   test_map();
+  test_join();
   test_filter();
   test_head();
   test_last();
+  test_tail();
+  test_init();
+  test_null();
+  test_at();
+  test_reverse();
 }
